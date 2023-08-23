eda131744c75e0bb7de34e044152ea4b9502702e
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-23 14:24:37 -0500

    wrote calc and updated greet doc strings

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,42 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+@app.get("/add")
+def query_add():
+    """returns addition result from query"""
+    queries = process_query_to_int(request)
+    result = add(queries[0], queries[1])
+    return str(result)
+
+
+@app.get("/sub")
+def query_sub():
+    """returns subtraction result from query"""
+    queries = process_query_to_int(request)
+    result = sub(queries[0], queries[1])
+    return str(result)
+
+
+@app.get("/mult")
+def query_multiply():
+    """returns multiplication result from query"""
+    queries = process_query_to_int(request)
+    result = mult(queries[0], queries[1])
+    return str(result)
+
+
+@app.get("/div")
+def query_division():
+    """returns division result from query"""
+    queries = process_query_to_int(request)
+    result = int(div(queries[0], queries[1]))
+    return str(result)
+
+
+def process_query_to_int(request):
+    """returns query params as integer tuple"""
+    return (int(request.args["a"]), int(request.args["b"]))
--- greet/app.py
+++ greet/app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.get('/welcome')
 def welcome():
-
+    """incredible welcome message"""
     return f"<h1> Welcome </h1>"
 
 
 @app.get('/welcome/home')
 def welcome_home():
-
+    """soothing return home"""
     return f"<h1> Welcome home </h1>"
 
 
 @app.get('/welcome/back')
 def welcome_back():
-
+    """You're back!"""
     return f"<h1> Welcome back </h1>"
